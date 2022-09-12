d80667fc224f59295fb5e88ec62fcb5cede5709d
eduardojigub <eduardobrunoit@gmail.com>
2022-09-12 18:29:44 -0300

    installing axios and fetching data

--- crypto-react-js/src/App.js
+++ crypto-react-js/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+      (response) => {
+        console.log(response.data);
+      }
+    );
+  }, []);
   return (
     <div className='App'>
-      <h1>Teste</h1>
+      <header className='cryptoHeader'></header>
+      <div classname='cryptoDisplay'></div>
     </div>
   );
 }
